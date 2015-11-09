d1dd7963ad037dc58c5beb8f3978f9da5afc9ad3
Chris Spradling <cjspradling@gmail.com>
2015-11-08 22:50:17 -0600

    finished registration validation (register page needs formatting work)

--- cgn.js
+++ cgn.js
@@ -10,38 +10,28 @@ $(document).ready(function () {
 
     $("#regButton").click( function  () {
         var fields = document.getElementById("regForm");
-        var username = $("#username").value;
+        var username = fields.username.value;
         var pass = fields.pass.value;
         var pass_repeat = fields.pass_repeat.value;
         if (username.length > 5 && username.length < 11) {
-            alert("userlength");
             if (username[0].match(/[a-z]/i)) {
-                alert("user init match");
                 if (~username.match(/^[a-z0-9]+$/i)) {
-                    alert('user az match');
                     if (pass == pass_repeat) {
-                        alert("passmatch");
                         if (5 < pass.length && pass.length < 11) {
-                            alert("passlength ok");
                             if (~pass.match(/^[a-z0-9_]+$/i)) {
-                                alert("pass not nonalnum");
                                 if (pass.match(/[a-z]/)) {
-                                    alert("pass has a lower");
                                     if (pass.match(/[A-Z]/)) {
-                                        alert("pass has a upper");
                                         if (pass.match(/[0-9]/)) {
-                                            alert("Passed Validation");
                                             return;
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        alert("Invalid Input");
+                                        } else {alert("Password must have at least one number");}
+                                    } else {alert("Password must have at least one uppercase letter");}
+                                } else {alert("Password must have at least one lowercase letter");}
+                            } else {alert("Password can only contain letters, digits, and underscore ( _ )");}
+                        } else {alert("Password should be between 6 and 10 characters");}
+                    } else {alert("Passwords do not match");}
+                } else {alert('Username can only contain letters and digits');}
+            } else {alert("Username must begin with a letter");}
+        } else {alert("Username should be between 6 and 10 characters");}
     });
 
 
