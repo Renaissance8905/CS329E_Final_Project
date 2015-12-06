7c8f9b5d789215b339e72f0de085579b32555a7b
Ke Chen <CK@QWERT-2.local>
2015-12-05 20:48:28 -0600

    cssfix

--- contact.php
+++ contact.php
@@ -300,22 +300,22 @@ function contact(){
         
     }
    print<<<PAGE
-   <div id="contact-page" class="container">
-    	<div class="bg">
+   <div class="bg">
 	    	<div class="row">    		
 	    		<div class="col-sm-12">    			   			
 					<h2 class="title text-center">Contact <strong>Us</strong></h2>    			    				    				
 					<div id="gmap" class="contact-map">
-                    <img id = "contactimg" src ="feedback.jpg" alt =""/>
+                    <img src = "feedback.jpg" alt ="" id = contactimg>
 					</div>
 				</div>			 		
-			</div>    	
+			</div>    
+        <div id="contact-page" class="container">
     		<div class="row">  	
 	    		<div class="col-sm-8">
 	    			<div class="contact-form">
 	    				<h2 class="title text-center">Feedback</h2>
 	    				<div class="status alert alert-success" style="display: none"></div>
-				    	<form id="contactform" class="contact-form row" name="contact-form" method="post">
+				    	<form id="main-contact-form" class="contact-form row" name="contact-form" method="post">
 				            <div class="form-group col-md-6">
 				                <input type="text" name="name" class="form-control" required="required" placeholder="Name">
 				            </div>
@@ -326,8 +326,8 @@ function contact(){
 				                <input type="text" name="subject" class="form-control" required="required" placeholder="Subject">
 				            </div>
 				            <div class="form-group col-md-12">
-				                <textarea name="comments" id="comments" required="required" class="form-control" rows="8" placeholder="Your Message Here"></textarea>
-				            <div><span id="advisor"></span></div>                       
+				                <textarea name="comments" id="message" required="required" class="form-control" rows="8" placeholder="Your Message Here"></textarea>
+				            </div>                        
 				            <div class="form-group col-md-12">
 				                <input type="submit" name="submit" class="btn btn-primary pull-right" value="Submit">
 				            </div>
